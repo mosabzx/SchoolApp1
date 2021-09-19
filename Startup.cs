7d591ffd6f1cfbd286628f508487fb9c8b82b98a
Mosab Al Sherawy <mosabzx@hotmail.com>
2021-09-19 09:21:54 +0200

    Deleting StudentCourseViewModel, unnecessary model class

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SchoolApp.Data;
 using SchoolApp.Models;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +33,7 @@ namespace SchoolApp
             services.AddScoped<Student>();
             services.AddScoped<Teacher>();
             services.AddScoped<StudentCourse>();
+            
 
 
             services.AddDbContext<SchoolDb>(options =>
